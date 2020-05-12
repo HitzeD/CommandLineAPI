cc5e21c73f2628f17f9400017837c34e77f11532
HitzeD <dakotahhitze@gmail.com>
2020-05-12 18:35:25 -0400

    setup server properly

--- api/server.js
+++ api/server.js
@@ -1,5 +1,15 @@
 const express = require('express');
-const cors = require('cors');
+const helmet = require('helmet');
+
+const commandsRouter = require('./commands/commandsRouter.js');
+
 const server = express();
 
-server.get('/', (req, res) => res.status(200).json({ title: 'Server Running'}));
+server.use(helmet());
+server.use(express.json());
+
+server.use('/commands', commandsRouter);
+
+server.get('/', (req, res) => res.status(200).json('Server Running'));
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+
 const server = require('./api/server.js');
 
-server.listen(5000, () => console.log('Server running on port 5000'));
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => console.log(`*** Server running on http://localhost:${port}/`));
