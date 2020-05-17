4ed16b0a9fe8499f8070d912d8948cae8fff3859
HitzeD <dakotahhitze@gmail.com>
2020-05-16 22:50:49 -0400

    hopefully it fixes the issue

--- api/server.js
+++ api/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-const helmet = require('helmet');
+// const helmet = require('helmet');
+const server = express();
+
+server.use(require('body-parser').text());
+
 
 const commandsRouter = require('./commands/commandsRouter.js');
 
-const server = express();
 
-server.use(helmet());
+// server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
