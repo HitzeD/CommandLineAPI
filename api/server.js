ecff655ae7c447f4ac2d3347a4ff407312551153
Dakotah Hitze <d-hitze@comcast.net>
2020-06-04 13:57:46 -0400

    added cors

--- api/server.js
+++ api/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const helmet = require('helmet');
+const server = express();
+
+server.use(require('body-parser').text());
 
 const commandsRouter = require('./commands/commandsRouter.js');
 
 const server = express();
 
-server.use(helmet());
+server.use(helmet()); // sets some HTTP headers
+server.use(cors());
 server.use(express.json());
 
 server.use('/commands', commandsRouter);
